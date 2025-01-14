0aab9af2d82f3ee077730f3017384db1eda31096
brookshu <134887313+brookshu@users.noreply.github.com>
2025-01-13 21:24:55 -0600

    profile page template

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,17 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 const Profile = () => {
-  return <h1 className="text-6xl font-bold">Profile</h1>
+  return (
+    <div className="min-h-screen flex flex-col items-center pt-8">
+      <Avatar className="w-32 h-32 mb-4">
+        <AvatarImage src="src/assets/blank-pfp.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <h1 className="text-2xl font-bold">Name</h1>
+    </div>
+  
+  )
+  
 }
 
 export default Profile
