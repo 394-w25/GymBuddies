a2789d328b693b981d2e64bbb9df95900d126a9a
chip <49256163+chipanyanwu@users.noreply.github.com>
2025-01-14 20:56:59 -0600

    put posts in a list

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -2,19 +2,47 @@ import WorkoutCard from "@/components/common/WorkoutCard"
 
 const Feed = () => {
   return (
-    <WorkoutCard
-      username="Chip"
-      title="Great workout today"
-      caption="I can't walk anymore"
-      date={new Date()}
-      durationInMinutes={80}
-      volume={10000}
-      exercises={[
-        { name: "Dumbbell Curls", sets: [] },
-        { name: "Bench Press", sets: [] },
-        { name: "Squats", sets: [] },
-      ]}
-    />
+    <div className="flex flex-col gap-4">
+      <PostCard
+        username="Chip"
+        title="Great workout today"
+        caption="I can't walk anymore"
+        date={new Date()}
+        durationInMinutes={80}
+        volume={10000}
+        exercises={[
+          { name: "Dumbbell Curls", sets: [] },
+          { name: "Bench Press", sets: [] },
+          { name: "Squats", sets: [] },
+        ]}
+      />
+      <PostCard
+        username="Chip"
+        title="Great workout today"
+        caption="I can't walk anymore"
+        date={new Date()}
+        durationInMinutes={80}
+        volume={10000}
+        exercises={[
+          { name: "Dumbbell Curls", sets: [] },
+          { name: "Bench Press", sets: [] },
+          { name: "Squats", sets: [] },
+        ]}
+      />
+      <PostCard
+        username="Chip"
+        title="Great workout today"
+        caption="I can't walk anymore"
+        date={new Date()}
+        durationInMinutes={80}
+        volume={10000}
+        exercises={[
+          { name: "Dumbbell Curls", sets: [] },
+          { name: "Bench Press", sets: [] },
+          { name: "Squats", sets: [] },
+        ]}
+      />
+    </div>
   )
 }
 
