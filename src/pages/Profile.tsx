e7b4aed179f3a40ef74aec1f23c7024b824755dc
chip <49256163+chipanyanwu@users.noreply.github.com>
2025-01-14 20:39:06 -0600

    fix profile page styling

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,16 +2,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const Profile = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center pt-8">
+    <div className="flex flex-col items-center">
       <Avatar className="w-32 h-32 mb-4">
         <AvatarImage src="src/assets/blank-pfp.png" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
       <h1 className="text-2xl font-bold">Name</h1>
     </div>
-  
   )
-  
 }
 
 export default Profile
