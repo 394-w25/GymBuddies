052efb09bd2bb85e67aa61cda34e001e4040908c
chip <49256163+chipanyanwu@users.noreply.github.com>
2025-01-15 03:58:11 -0600

    connect profile page to user profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,20 @@
+import { useUser } from "@/components/Layout/UserContext"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const Profile = () => {
+  const { user } = useUser()
   return (
-    <div className="flex flex-col items-center">
-      <Avatar className="w-32 h-32 mb-4">
-        <AvatarImage src="src/assets/blank-pfp.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <h1 className="text-2xl font-bold">Name</h1>
-    </div>
+    <>
+      {user && (
+        <div className="flex flex-col items-center">
+          <Avatar className="w-32 h-32 mb-4">
+            <AvatarImage src={user.profilePic} />
+            <AvatarFallback>{user.name.at(0)}</AvatarFallback>
+          </Avatar>
+          <h1 className="text-2xl font-bold">{user.name}</h1>
+        </div>
+      )}
+    </>
   )
 }
 
