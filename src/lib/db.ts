5ee7215cd7d6d3677219a9819ba395f8dc0d28aa
davi-maciel <70175298+davi-maciel@users.noreply.github.com>
2025-01-20 17:34:41 -0600

    Implement "streak" update
    For now it's just counting the total number of workouts a user has

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from "uuid"
 import type { User } from "@/types/user"
 import type { User as FirebaseUser } from "firebase/auth"
 import type { Workout, WorkoutLog } from "@/types/workout"
+import { data } from "react-router"
 
 export const addUser = async (user: FirebaseUser) => {
   const { uid, photoURL, displayName, email } = user
@@ -54,7 +55,9 @@ export const addWorkout = async (userId: string, workout: WorkoutLog) => {
       ? [...userWorkoutsSnapshot.val(), workoutId]
       : [workoutId]
 
-    await update(ref(database, `users/${userId}`), { workouts: userWorkouts })
+    const userData = userWorkoutsSnapshot.val()
+    await update(ref(database, `users/${userId}`), { workouts: userWorkouts, 
+      streak : userData.streak + 1})
 
     console.log(`Workout ${workoutId} added for user ${userId}`)
 
@@ -173,5 +176,99 @@ export const getAllWorkouts = async (): Promise<Workout[]> => {
   } catch (err) {
     console.error(`An error occurred while fetching all workouts:`, err)
     return []
+  }
+}
+
+export const updateWorkout = 
+async (userId : string, workoutId : string, workout : WorkoutLog) => {
+  if (workout.endTime) {
+    const workoutData: Workout = {
+      workoutId,
+      userId: userId,
+      title: workout.title,
+      caption: workout.caption,
+      date: workout.date,
+      startTime: workout.startTime,
+      endTime: workout.endTime,
+      exercises: workout.exercises,
+    }
+
+    try {
+      await set(ref(database, `workouts/${workoutId}`), workoutData);
+      return true;
+
+    } catch (err) {
+      console.log(`could not update finalized workout : `, err);
+      return false;
+    }
+
+  } else {
+    try {
+      await update(ref(database, `workouts/${workoutId}`), workout);
+      return true;
+
+    } catch (err) {
+      console.log(`could not update workout : `, err);
+      return false;
+    }
+  }
+}
+
+export const updateUserStatus = async (userId : string, status: boolean) => {
+  try {
+    await update(ref(database, `users/${userId}`), {status : status});
+    return true;
+  } catch (err) {
+    console.log(`failed to update user ${userId}`, err);
+    return false;
+  }
+}
+
+export const addFriend = async (userId :string, friendId: string) => {
+
+  //add friend's id to user friend list
+  get(ref(database, `users/${userId}`)).then((data) => {
+    if (!data.exists()) {
+      // no data, no pass
+      throw new Error("Failed to fetch user from database :(");
+    }
+    const currUser = data.val();
+    update(ref(database, `users/${userId}`), {friends : ((currUser.friends !== undefined) ? [...currUser.friends, friendId] : [friendId] ) })
+  }).catch((err) => {
+    console.log(`error ocurred while trying to look up user ${userId}`, err);
+    return false;
+  }) 
+
+  // right now just force both people to be friends
+
+  get(ref(database, `users/${friendId}`)).then((data) => {
+    if(!data.exists()) {
+      throw new Error("Failed to fetch friend from db :(");
+    } else {
+      const currFriend = data.val();
+      update(ref(database, `users/${friendId}`), {friends : ((currFriend.friends !== undefined) ? [...currFriend.friends, userId] : [userId] ) })
+
+    }
+  }).catch((err) => {
+    console.log(`Could not complete friend add, likely could not find friend with friend id ${friendId}`, err);
+    return false;
+  });
+
+  console.log(`successfully linked friends ${userId} and ${friendId}`);
+  return true;
+
+}
+
+export const updateStreak = async (userId: string) => {
+  try {
+    const userRef = ref(database, `users/${userId}`)
+    const snapshot = await get(userRef)
+    if (snapshot.exists()) {
+      const userData = snapshot.val()
+      await update(userRef, {...userData, streak : userData.streak + 1})
+    }
+  } catch (err) {
+    console.log(`failed to update streak for user ${userId}`, err);
+    return false;
   }
 }
